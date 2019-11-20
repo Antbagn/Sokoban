a36e45379218205126620e47c6eb0a473a921e41
Anton Bagnykov <antbagn222@mail.ru>
2019-11-20 21:57:06 +0300

    redactor kart

--- SokobanEditor/SokobanEditor/SokobanEditor.Designer.cs
+++ SokobanEditor/SokobanEditor/SokobanEditor.Designer.cs
@@ -28,21 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SokobanEditor));
+            this.panel = new System.Windows.Forms.Panel();
+            this.toolWall = new System.Windows.Forms.ToolStripButton();
+            this.toolAbox = new System.Windows.Forms.ToolStripButton();
+            this.toolHere = new System.Windows.Forms.ToolStripButton();
+            this.toolDone = new System.Windows.Forms.ToolStripButton();
+            this.toolNone = new System.Windows.Forms.ToolStripButton();
+            this.toolUser = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel.Location = new System.Drawing.Point(0, 39);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(593, 329);
+            this.panel.TabIndex = 1;
+            // 
+            // toolWall
+            // 
+            this.toolWall.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolWall.Image = global::SokobanEditor.Properties.Resources.wall;
+            this.toolWall.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolWall.Name = "toolWall";
+            this.toolWall.Size = new System.Drawing.Size(36, 36);
+            this.toolWall.Text = "toolStripButton1";
+            // 
+            // toolAbox
+            // 
+            this.toolAbox.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolAbox.Image = global::SokobanEditor.Properties.Resources.abox;
+            this.toolAbox.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolAbox.Name = "toolAbox";
+            this.toolAbox.Size = new System.Drawing.Size(36, 36);
+            this.toolAbox.Text = "toolStripButton2";
+            // 
+            // toolHere
+            // 
+            this.toolHere.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolHere.Image = global::SokobanEditor.Properties.Resources.here;
+            this.toolHere.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolHere.Name = "toolHere";
+            this.toolHere.Size = new System.Drawing.Size(36, 36);
+            this.toolHere.Text = "toolStripButton3";
+            // 
+            // toolDone
+            // 
+            this.toolDone.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolDone.Image = global::SokobanEditor.Properties.Resources.done;
+            this.toolDone.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolDone.Name = "toolDone";
+            this.toolDone.Size = new System.Drawing.Size(36, 36);
+            this.toolDone.Text = "toolStripButton4";
+            // 
+            // toolNone
+            // 
+            this.toolNone.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolNone.Image = global::SokobanEditor.Properties.Resources.none;
+            this.toolNone.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolNone.Name = "toolNone";
+            this.toolNone.Size = new System.Drawing.Size(36, 36);
+            this.toolNone.Text = "toolStripButton5";
+            // 
+            // toolUser
+            // 
+            this.toolUser.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolUser.Image = global::SokobanEditor.Properties.Resources.user1;
+            this.toolUser.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolUser.Name = "toolUser";
+            this.toolUser.Size = new System.Drawing.Size(36, 36);
+            this.toolUser.Text = "toolStripButton6";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolWall,
+            this.toolAbox,
+            this.toolHere,
+            this.toolDone,
+            this.toolNone,
+            this.toolUser,
+            this.toolStripSeparator1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(593, 39);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.ToolStrip1_ItemClicked);
             // 
             // SokobanEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(593, 368);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SokobanEditor";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Редактор уровней игры sokoban";
             this.Load += new System.EventHandler(this.SokobanEditor_Load);
+            this.Resize += new System.EventHandler(this.SokobanEditor_Resize);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.ToolStripButton toolWall;
+        private System.Windows.Forms.ToolStripButton toolAbox;
+        private System.Windows.Forms.ToolStripButton toolHere;
+        private System.Windows.Forms.ToolStripButton toolDone;
+        private System.Windows.Forms.ToolStripButton toolNone;
+        private System.Windows.Forms.ToolStripButton toolUser;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
     }
 }
 
--- SokobanEditor/SokobanEditor/SokobanEditor.cs
+++ SokobanEditor/SokobanEditor/SokobanEditor.cs
@@ -22,17 +22,107 @@ namespace SokobanEditor
     };
     public partial class SokobanEditor : Form
     {
-        
+        PictureBox[,] box;
+        Cell[,] cell;
+        int widht, height;
+
         public SokobanEditor()
         {
+
             InitializeComponent();
         }
 
         private void SokobanEditor_Load(object sender, EventArgs e)
         {
-            Cell[,] cell;
+           
+            
             LevelFile level=new LevelFile("levels.txt");
-            cell = level.loadLevel(2);
+            cell = level.loadLevel(3);
+            widht =cell.GetLength(0);
+            height = cell.GetLength(1);
+            InitPictures();
+            LoadPictures();
+        }
+        public void InitPictures()
+        {
+            int bw, bh;
+            bw = panel.Width / widht;
+            bh = panel.Height / height;
+            if (bw < bh) bh = bw;
+            else bw = bh;
+
+            box = new PictureBox[widht, height];
+            for(int x=0;x<widht;x++)
+                for(int y=0;y<height;y++)
+                {
+                    PictureBox picture = new PictureBox();
+                    picture.BackColor = System.Drawing.Color.White;
+                    picture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    picture.Location = new System.Drawing.Point(x*(bw-1),y*(bh-1));
+                    picture.Size = new System.Drawing.Size(bw,bh);
+                    picture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                    picture.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseClick);
+                    panel.Controls.Add(picture);
+                    box[x, y] = picture;
+                    
+                }
+        }
+        public void LoadPictures()
+        {
+            for (int x = 0; x < widht; x++)
+                for (int y = 0; y < height; y++)
+                {
+                    box[x, y].Image = CellToPicture(cell[x, y]);
+
+                }
+        }
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+
+            }
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+
+            }
+        }
+        private Image CellToPicture(Cell c)
+        {
+            switch (c)
+            {
+                case Cell.none:return Properties.Resources.none;
+                case Cell.abox: return Properties.Resources.abox;
+                case Cell.wall: return Properties.Resources.wall;
+                case Cell.user: return Properties.Resources.user1;
+                case Cell.done: return Properties.Resources.done;
+                case Cell.here: return Properties.Resources.here;
+                default:return Properties.Resources.none;
+            }
+        }
+
+        private void SokobanEditor_Resize(object sender, EventArgs e)
+        {
+            int bw, bh;
+            bw = panel.Width / widht;
+            bh = panel.Height / height;
+            if (bw < bh) bh = bw;
+            else bw = bh;
+
+            
+            for (int x = 0; x < widht; x++)
+                for (int y = 0; y < height; y++)
+                {
+                    box[x, y].Size = new System.Drawing.Size(bw,bh);
+                    box[x, y].Location = new System.Drawing.Point(x * (bw - 1), y * (bh - 1));
+
+                }
+        }
+
+        private void ToolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
         }
     }
 }
